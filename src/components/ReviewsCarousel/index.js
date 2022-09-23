9f56948f0eb7681a83a12a59d9c79b9d5b71388a
Vipin Patel <112804686+Vipin-patelME@users.noreply.github.com>
2022-09-23 12:22:33 +0530

    Update index.js

--- src/components/ReviewsCarousel/index.js
+++ src/components/ReviewsCarousel/index.js
@@ -3,71 +3,67 @@ import {Component} from 'react'
 import './index.css'
 
 const ReviewComponent = props => {
-  const {eachReview, rightuserslide} = props
-  const {imgUrl, username, companyName, description} = eachReview
-
-  const onRightSlide = () => {
-    rightuserslide(username)
-  }
+  const {filteredUserReview} = props
+  const {imgUrl, username, companyName, description} = filteredUserReview
 
   return (
     <li className="userList">
-      <button type="button">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
-          alt="left arrow"
-          className="arrow-style"
-        />
-      </button>
       <div>
         <img src={imgUrl} alt={username} />
         <p>{username}</p>
         <p>{companyName}</p>
         <p>{description}</p>
       </div>
-      <button type="button">
-        <img
-          src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
-          alt="right arrow"
-          className="arrow-style"
-          onClick={onRightSlide}
-        />
-      </button>
     </li>
   )
 }
 
 class ReviewsCarousel extends Component {
-  state = {userName: 'Wade Warren'}
+  state = {count: 0}
 
-  rightuserslide = username => {
-    this.setState({userName: username})
+  onRightSlide = () => {
+    this.setState(PrevState => ({
+      count: PrevState.count < 3 ? PrevState.count + 1 : 3,
+    }))
   }
 
-  filteredReviewList = reviewsList => {
-    const {userName} = this.state
-    const filteredReviews = reviewsList.filter(
-      eachReviews => eachReviews.username === userName,
-    )
-    return filteredReviews
+  onLeftSlide = () => {
+    this.setState(PrevState => ({
+      count: PrevState.count < 1 ? 0 : PrevState.count - 1,
+    }))
   }
 
   render() {
+    const {count} = this.state
     const {reviewsList} = this.props
-    const filteredUserReview = this.filteredReviewList(reviewsList)
+    const lengthOfReviewList = reviewsList.length
+    const filteredUserReview =
+      lengthOfReviewList >= count
+        ? reviewsList[count]
+        : reviewsList[lengthOfReviewList - 1]
+    console.log(count)
+    console.log(lengthOfReviewList)
     console.log(filteredUserReview)
     return (
       <div className="main-bg">
         <h1>Reviews</h1>
-        <ul className="list-container">
-          {filteredUserReview.map(eachReview => (
-            <ReviewComponent
-              eachReview={eachReview}
-              key={eachReview.username}
-              rightuserslide={this.rightuserslide}
+        <div className="list-container">
+          <button testid="leftArrow" type="button" onClick={this.onLeftSlide}>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+              alt="left arrow"
+              className="arrow-style"
             />
-          ))}
-        </ul>
+          </button>
+          <ReviewComponent filteredUserReview={filteredUserReview} />
+          <button type="button" testid="rightArrow" onClick={this.onRightSlide}>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+              alt="right arrow"
+              className="arrow-style"
+            />
+          </button>
+        </div>
       </div>
     )
   }
